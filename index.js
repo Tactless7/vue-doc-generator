0d5693c1eed8cee2e9595097e57bffb67b8bf26f
leo <leo@schoolmouv.fr>
2018-05-25 17:32:36 +0200

    Start of commander

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 
+program
+	.version('1.0.0', '-v, --version')
+	.usage('<filename>')
+	.parse(process.argv);
 
+console.log(program.args);
